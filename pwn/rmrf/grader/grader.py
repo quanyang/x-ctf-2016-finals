2b3ab92b5622742dc03e4655a86eb4a5a5f56b59
amon <amon@nandynarwhals.org>
2016-06-18 02:20:35 +0800

    Correct flag

--- pwn/rmrf/grader/grader.py
+++ pwn/rmrf/grader/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, key):
-    if "C47ch_M3_If_Y0u_C4n" in key:
+    if "XCTF{DANKMEMESCANTMELTSTEELBEAMS}" in key:
         return True, "Very easy."
     else:
         return False, "Wrong."
